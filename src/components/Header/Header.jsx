334ed2820383ec53355caae22ca1a1e8b0021b5f
Dark-Fahim <itsmunna1066@gmail.com>
2025-07-01 12:51:06 +0600

    test

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,45 @@
+import { CgProfile } from "react-icons/cg";
+
+const Header = () => {
+    return (
+        <div>
+            <div className="navbar bg-base-100 shadow-sm">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+                        </div>
+                        <ul
+                            tabIndex={0}
+                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                            <li><a>Home</a></li>
+                            <li>
+                                <a>Recipe</a>
+                            </li>
+                            <li><a>About</a></li>
+                            <li><a>Search</a></li>
+                        </ul>
+                    </div>
+                    <a className="btn btn-ghost text-xl">Recipe Calories</a>
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1">
+                        <li><a>Home</a></li>
+                        <li>
+                            <a href="">Recipe</a>
+                        </li>
+                        <li><a>About</a></li>
+                        <li><a>Search</a></li>
+                    </ul>
+                </div>
+                <div className="navbar-end">
+                    <input type="text" placeholder="Search" className="input rounded-full input-bordered w-24 md:w-auto" />
+                    <a className="btn bg-green-100 text-green-400"><CgProfile></CgProfile></a>
+                </div>
+            </div>
+            {/* Banner */}
+        </div>
+    );
+};
+
+export default Header;
